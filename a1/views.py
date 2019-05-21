c91aad67d68f47f5428f712667319bc759b0e6ca
John Salvador <john@WNRX38-02.ds.mot.com>
2019-05-21 11:34:04 -0700

    finished user database, added front end user app

--- a1/views.py
+++ a1/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-dummy_posts=[
-	{
-		'author':'john',
-		'title':'blog post 1',
-		'content': 'hello',
-		'date_posted':'May 8, 2018'
-	},
-	{
-		'author':'jane',
-		'title':'blog post 2',
-		'content':'hi',
-		'date_posted':'May 9, 2019'
-	}
-]
 def home(request):
 	context = {
-		'posts':dummy_posts
+		'posts':Post.objects.all()
 	}
 	return render(request, 'a1/home.html', context)
 
